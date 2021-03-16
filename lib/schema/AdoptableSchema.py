41d16fe9f5637adbe315a89f5af704f35b1cee2f
BTaapken <beau@lioncode.nl>
2021-03-16 12:01:59 +0100

    * Removed allAdoptables * Renamed pagination_adoptables to adoptable * database only gets filled with data if on development environment * Added a lot of values that should be in env variables to env file

--- lib/schema/AdoptableSchema.py
+++ lib/schema/AdoptableSchema.py
@@ -11,17 +11,13 @@ class Adoptable(MongoengineObjectType):
 
 
 class Query(graphene.ObjectType):
-    all_adoptables = graphene.List(Adoptable)
-    pagination_adoptables = graphene.List(
+    adoptable = graphene.List(
         Adoptable,
         page=graphene.Int(),
         limit=graphene.Int()
     )
 
-    def resolve_all_adoptables(self, info):
-        return list(AdoptableModel.objects.order_by('-id').all())
-
-    def resolve_pagination_adoptables(self, info, page, limit):
+    def resolve_adoptable(self, info, page, limit):
         adoptables_to_skip = page * limit
         return list(AdoptableModel.objects.order_by('-id').skip(adoptables_to_skip).limit(limit))
 
--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from mongoengine import connect
 from flask_graphql import GraphQLView
 from lib.schema.AdoptableSchema import schema
 from populate_db import populate
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
 
 app = Flask(__name__)
 CORS(app)
@@ -17,7 +22,8 @@ app.add_url_rule('/graphql', view_func=GraphQLView.as_view(
 
 
 if __name__ == '__main__':
-    db = connect('RepoAdopt', host='mongodb://localhost/', port=27017, alias='default')
-    db.drop_database('RepoAdopt')
-    populate()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    db = connect('RepoAdopt', host=getenv("HOST"), port=getenv("DBPORT"), alias='default')
+    if getenv("ENVIRONMENT") == "develop":
+        db.drop_database('RepoAdopt')
+        populate()
+    app.run(debug=True, host='0.0.0.0', port=getenv("PORT"))
