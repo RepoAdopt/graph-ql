236d3d1144f4aba4a70e39aeff3cdfdc5ea15c90
Niek125 <niek.sleddens@gmail.com>
2021-03-30 11:31:42 +0200

    add auth middleware

--- lib/adoptable/mutation.py
+++ lib/adoptable/mutation.py
@@ -1,7 +1,4 @@
-import json
 import graphene
-
-from jwcrypto.jws import JWS
 
 from .type import AdoptableType
 from .model import Adoptable
@@ -15,15 +12,8 @@ class CreateAdoptable(graphene.Mutation):
         repository = graphene.String(required=True)
         description = graphene.String(required=False)
 
-    def mutate(root, info, **args):
-        token = JWS()
-        token.deserialize(info.context.headers['Authorization'].removeprefix('Bearer '))
-        
-        byte_payload = token.objects.pop('payload')
-        json_payload = byte_payload.decode('UTF-8')
-        payload = json.loads(json_payload)
-
-        adoptable = Adoptable(**args, owner=payload['username'])
+    def mutate(root, info, token, **args):
+        adoptable = Adoptable(**args, owner=token['username'])
         adoptable.save()
 
         return CreateAdoptable(adoptable=adoptable)
