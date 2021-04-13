da157b946f930104672ecf2e13f908788a80119f
Niek125 <niek.sleddens@gmail.com>
2021-04-13 15:52:36 +0200

    fix populate

--- lib/match/populate.py
+++ lib/match/populate.py
@@ -2,9 +2,9 @@ from .model import Match
 
 
 def populate():
-    match = Match(repository="60756a6b2ef13b728ec4cc29", user="Niek125")
+    match = Match(repository_id="60756a6b2ef13b728ec4cc29", user="Niek125")
     match.save()
-    match1 = Match(repository="60756a6b2ef13b728ec4cc2a", user="BeauTaapken")
+    match1 = Match(repository_id="60756a6b2ef13b728ec4cc2a", user="BeauTaapken")
     match1.save()
-    match2 = Match(repository="60756a6b2ef13b728ec4cc2b", user="Niek125")
+    match2 = Match(repository_id="60756a6b2ef13b728ec4cc2b", user="Niek125")
     match2.save()
