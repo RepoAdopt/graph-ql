6905109b26cb10d8a2da2957cffcc56163e6bead
BTaapken <beau@lioncode.nl>
2021-04-13 13:11:23 +0200

    * Fixed bug where backend allowed for user to create the same adoptable multiple times and where user could match with adoptables mutiple times

--- lib/adoptable/model.py
+++ lib/adoptable/model.py
@@ -5,7 +5,10 @@ from mongoengine.fields import (
 
 
 class Adoptable(Document):
-    meta = {"collection": "adoptables"}
+    meta = {
+        "collection": "adoptables",
+        "indexes": [{"fields": ("repository", "owner"), "unique": True}],
+    }
     repository = StringField(required=True)
     description = StringField(required=False)
     owner = StringField(required=True)
--- lib/match/model.py
+++ lib/match/model.py
@@ -3,6 +3,9 @@ from mongoengine.fields import StringField, ObjectIdField
 
 
 class Match(Document):
-    meta = {"collection": "matches"}
+    meta = {
+        "collection": "matches",
+        "indexes": [{"fields": ("repository", "user"), "unique": True}],
+    }
     repository = ObjectIdField(required=True)
     user = StringField(required=True)
