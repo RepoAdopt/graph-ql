d936d7fee869461425e93e1e7d51e4e2b9731505
Niek125 <niek.sleddens@gmail.com>
2021-04-20 10:42:28 +0200

    retry

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from time import sleep, time_ns
 from requests import post
 from flask import Flask
 from flask_cors import CORS
@@ -27,6 +28,36 @@ app.add_url_rule(
 )
 
 
+def establish_gateway_connection():
+    base_url = f'http://{getenv("GATEWAY")}/services'
+    name = "graphql"
+    try:
+        res = post(
+            url=base_url,
+            data={"name": name, "url": getenv("GRAPHQL_URL")},
+        )
+
+        if res.status_code == 201:
+            post(
+                url=f"{base_url}/{name}/routes",
+                data={"name": name, "paths[]": "/graphql"},
+            )
+
+            print("Created gateway connection!")
+
+            return
+        elif res.status_code == 409:
+            print("Gateway connection already created!")
+        else:
+            print("Could not create gateway connection!")
+
+    except:
+        print("Gateway is not available!")
+
+    sleep(time_ns=200)
+    establish_gateway_connection()
+
+
 if __name__ == "__main__":
     db = connect(
         "RepoAdopt", host=getenv("HOST"), port=getenv("DBPORT"), alias="default"
@@ -35,22 +66,6 @@ if __name__ == "__main__":
         db.drop_database("RepoAdopt")
         populate()
 
-    base_url = f'http://{getenv("GATEWAY")}/services'
-    name = "graphql"
-    res = post(
-        url=base_url,
-        data={"name": name, "url": getenv("GRAPHQL_URL")},
-    )
-
-    if res.status_code == 201:
-        post(
-            url=f"{base_url}/{name}/routes", data={"name": name, "paths[]": "/graphql"}
-        )
-
-        print("Created gateway connection!")
-    elif res.status_code == 409:
-        print("Gateway connection already created!")
-    else:
-        print("Could not create gateway connection!")
+    establish_gateway_connection()
 
     app.run(debug=True, host="0.0.0.0", port=getenv("PORT"))
