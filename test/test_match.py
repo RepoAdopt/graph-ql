22304d0fe8ea7c3df2532ce86c86a93c6d4ffe36
BTaapken <beau@lioncode.nl>
2021-04-20 10:20:01 +0200

    * Fixed test

--- test/test_match.py
+++ test/test_match.py
@@ -11,13 +11,13 @@ class MyTestCase(unittest.TestCase):
     def setUpClass(cls):
         connect("RepoAdoptTest", host="mongomock://localhost/")
 
-        adoptable = Adoptable(repository="testRepo", owner="test")
+        adoptable = Adoptable(repository="testRepo", owner="Test")
         adoptable.save()
-        adoptable1 = Adoptable(repository="testRepo1", owner="test")
+        adoptable1 = Adoptable(repository="testRepo1", owner="Test")
         adoptable1.save()
-        match = Match(user="test", adoptable=adoptable.id)
+        match = Match(user="Test", adoptable=adoptable.id)
         match.save()
-        match1 = Match(user="test", adoptable=adoptable1.id)
+        match1 = Match(user="Test", adoptable=adoptable1.id)
         match1.save()
 
     @classmethod
@@ -26,4 +26,6 @@ class MyTestCase(unittest.TestCase):
 
     def test_match_model(self):
         match = Match.objects().first()
+        print(match.user)
+        print(match.adoptable.repository)
         assert match.user == "Test" and match.adoptable.repository == "testRepo"
