3347d26dd4407a8acafdc26404d197e18004f1c2
Niek125 <niek.sleddens@gmail.com>
2021-05-12 13:55:25 +0200

    fix posting a message

--- lib/chatMessage/mutation.py
+++ lib/chatMessage/mutation.py
@@ -1,5 +1,7 @@
 import graphene
 
+from bson import ObjectId
+
 from datetime import datetime
 
 from .type import ChatMessageType
@@ -11,15 +13,16 @@ class PostChatMessage(graphene.Mutation):
 
     class Arguments:
         message = graphene.String(required=True)
+        chat_id = graphene.String(required=True)
 
-    def mutate(root, info, **args):
+    def mutate(root, info, chat_id, **args):
         token = getattr(info.context, "token", None)
 
         if token is None:
             return {}
 
         chat_message = ChatMessage(
-            **args, user=token["username"], timestamp=datetime.now()
+            **args, user=token["username"], timestamp=datetime.now(), chat_id=ObjectId(chat_id)
         )
         chat_message.save()
         return PostChatMessage(chat_message=chat_message)
